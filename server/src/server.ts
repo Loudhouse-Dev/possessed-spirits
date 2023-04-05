8fc902c2dbced20b27f1c4126ecbefaf0b760be3
Loudhouse-Dev <contactme@johnnyhowell.dev>
2023-04-05 16:05:20 -0500

    added routers

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,14 @@
 import express from 'express';
+import { Request, Response, NextFunction } from 'express';
 import router from './routes/cocktailRoutes';
 import morgan from 'morgan';
-
-
+import cors from 'cors';
+import { protect } from './modules/auth'
+import { createNewUser, signin } from './controllers/userController'
 
 const app = express();
 
+app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
@@ -14,6 +17,14 @@ app.get('/', (_req, res) => {
   res.send('Hello World!');
 });
 
-app.use('/api', router);
+app.use('/api', protect, router);
+
+app.post('/user', createNewUser)
+app.post('/signin', signin)
+
+app.use((err: Error, _req: Request, res: Response, _next: NextFunction) => {
+  console.log(err)
+  res.json({message: `had an error: ${err.message}`})
+})
 
 export default app;
